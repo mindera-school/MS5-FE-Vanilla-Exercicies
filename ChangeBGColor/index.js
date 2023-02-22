b2f2ab5526e9f31e1b69ac6f28230f514b77f9db
Adan0k <adansilvaoliveira@gmail.com>
2023-02-22 11:18:11 +0000

    Fix: BGcolor

---
+++ ChangeBGColor/index.js
@@ -0,0 +1,2 @@
+let button = document.querySelector("input");
+button.addEventListener("change", () => document.body.style.background = button.value);
--- ChangeBGColor/script.js
+++
@@ -1,6 +0,0 @@
-let a = document.getElementsByTagName("a");
-for(let i = 0; i < a.length; i++){
-    if(a[i].innerHTML == document.getElementById("title").innerHTML){
-       a[i].style.background = "red";
-    };
-}
