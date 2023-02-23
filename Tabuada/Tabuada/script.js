d8b63bac975831db2b1376be027d12721d40a34f
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-02-23 11:19:08 +0000

    add files tabuada

---
+++ Tabuada/Tabuada/script.js
@@ -0,0 +1,37 @@
+const button = document.getElementById("subBtn");
+const input = document.getElementById("multiplierInput");
+const form = document.getElementById("multiplierForm");
+const table = document.getElementById("tabuadaTable") 
+
+let number = "";
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
+
+button.addEventListener("click",()=>{
+    number = input.value; 
+    table.innerHTML = null;
+    const defTr= document.createElement("tr");
+    const defTh1 = document.createElement("th");
+    const defTh2 = document.createElement("th");
+    const defTh3 = document.createElement("th");
+    defTh1.innerHTML = "Multiplicador";
+    defTh2.innerHTML = "Multiplicando";
+    defTh3.innerHTML = "Resultado"
+
+    for (let i = 0; i < 11; i++) {
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        td.innerHTML = number;
+        const td2 = document.createElement("td");
+        td2.innerHTML = i;
+        const result = document.createElement("td");
+        result.innerHTML = number * i;
+        table.appendChild(tr);
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        tr.appendChild(result);
+    }
+});
+
