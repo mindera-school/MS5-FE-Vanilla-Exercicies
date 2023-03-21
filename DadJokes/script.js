f24de59bde758e72753c09dcc92e7e0b0a26d085
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-21 16:37:25 +0000

    Submit my work incomplete

---
+++ DadJokes/script.js
@@ -0,0 +1,22 @@
+const piadas = [
+    "Como se chama a neta do super Mário? -Marioneta",
+    "Quem é o mais velho, o sol ou a lua? -A Lua, porque já pode sair à noite.",
+    "Qual o nome do peixe que caiu do décimo andar? -Aaaaaaaaah, Tum!",
+    "O que é que uma mãe feiticeira diz à filha feiticeira? -Não devias ter feito isso!",
+    "O que diz um tomate para o outro? -Tu matas-me.",
+    "O que diz o tubarão para a “tubaroa”? -Tu baralhas-me.",
+    "Conheces a piada do iogurte? –É natural…",
+    "Porque é que ninguém se perde na Polónia? -Porque há muitas “polacas” de sinalização.",
+    "Sabem porque é que a pizza chora no funeral? -Porque é familiar.",
+    "Porque é que as pombas não matam os outros animais? -Porque têm pena.",
+    "Conheces o Mário???"
+  ];
+  
+  const piadaElement = document.getElementById('piada');
+  const botaoElement = document.getElementById('botao');
+  
+  botaoElement.addEventListener('click', function() {
+    const piadaAleatoria = piadas[Math.floor(Math.random() * piadas.length)];
+    piadaElement.innerHTML = piadaAleatoria;
+  });
+  
