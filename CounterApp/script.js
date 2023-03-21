ef28128f0e5f5095e561f5f4f62e8a18f2175f12
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-21 10:01:28 +0000

    remove console.log

--- CounterApp/script.js
+++ CounterApp/script.js
@@ -1,1 +1,14 @@
-console.log("smart")
+
+        const buttonMinus= document.getElementById("minus-button")
+        const buttonPlus= document.getElementById("plus-button")
+        const span= document.getElementById("value")
+        
+        let value= 0;
+        buttonMinus.addEventListener("click", ()=>{
+            value--;
+            span.textContent= value;
+        } )
+        buttonPlus.addEventListener("click", ()=>{
+            value++
+            span.textContent= value;
+        } )
